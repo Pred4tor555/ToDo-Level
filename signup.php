fa7397566ce2ab3af4f6ea607c6b13fa23d36e8d
Pred4tor555 <artuk200311@mail.ru>
2022-10-16 14:04:44 +0500

    test

---
+++ signup.php
@@ -0,0 +1,10 @@
+<?php
+    require 'configDB.php';
+?>
+
+<form action="/signup.php" method="POST">
+    <p>
+        <strong>Ваш логин</strong>
+        <input type="text" name="login">
+    </p>
+</form>
--- todo-level.js
+++ todo-level.js
@@ -1,11 +1,10 @@
-func () {
+fun1 () {
     var chbox;
     chbox = document.getElementById('one');
 
     if (chbox.checked) {
         alert('Выбрано');
     }
-
     else {
         alert('Не выбрано!');
     }
